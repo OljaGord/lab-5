d04d5137359ed9571a37eb763e3fcee027404dc6
Gordeeva Olga <olja.gord00@gmail.com>
2023-03-01 14:22:20 +0500

    lab-5

---
+++ src/main/java/com/example/mvctestapp/MvcTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvctestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcTestAppApplication.class, args);
+    }
+
+}
